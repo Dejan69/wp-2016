d8fb6f5e0f1e897eff9c93549e8e79cde107a91c
Mite Stojanov <mst@nix.mk>
2016-10-25 13:01:57 +0200

    lab 3

--- wp-starter-angular/app/directive/wp-input.js
+++ wp-starter-angular/app/directive/wp-input.js
@@ -1,31 +1,24 @@
 (function (angular) {
   'use strict';
 
-  angular
-    .module('wp-angular-starter')
-    .directive('wpInputHelper', function () {
+  angular.module('wp-angular-starter')
+    .directive('focusMe', function () {
         return {
-          restrict: 'EA',
-          scope: {
-            wpModel: '=wpModel',
-            wpType: '@wpType',
-            wpFocus: '@wpFocus'
-          },
-          template:
-          '<input class="form-control" type="{{wpType}}" ng-model="wpModel">',
-          link: function (scope, element, attrs) {
-            console.log(element);
-            if(attrs.wpFocus == 'true')
-              element[0].focus();
-            else
-              element[0].blur();
-          }
+	        link: function (scope, element, attrs) {
+		        var model = attrs.focusMe;
+		        scope.$watch(model, function (value) {
+			        console.log('value=', value);
+			        if (value == 'true') {
+				        element[0].focus();
+			        }
+		        });
+	        }
         }
     });
   angular.module('wp-angular-starter').component('wpInput', {
     template: '<div class="form-group">' +
     '<label>{{$ctrl.wpLabel}}</label>' +
-    '<wp-input-helper wp-model="vm.entity.property" wp-type="number" wp-focus="true"></wp-input-helper>' +
+    '<input ng-model="$ctrl.wpModel" type="{{$ctrl.wpType}}" focus-me="$ctrl.wpFocus"></wp-input-helper>' +
     '</div>',
     bindings: {
       wpModel: '=wpModel',
